f9b45b84f5dca8d91c6a85ff438c86d142272b54
Chazmuth <henryjobling@gmail.com>
2021-11-17 19:43:21 +0000

    version1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,10 @@ package com.company;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Random;
 
 public class Main {
 
@@ -10,14 +13,14 @@ public class Main {
         mainMenu();
     }
 
-    public static void mainMenu(){
-        try{
-            while(true){
+    public static void mainMenu() {
+        try {
+            while (true) {
                 System.out.println("What is your name?");
                 String userName = getInput();
                 System.out.println("Would You Like to Check a Ticket or Buy One?");
                 String menuChoice = getInput();
-                switch (menuChoice){
+                switch (menuChoice) {
                     case "Check":
                     case "check":
                         checkMenu(userName);
@@ -27,36 +30,63 @@ public class Main {
                         buyMenu(userName);
                 }
             }
-        }catch (Exception exception){
+        } catch (Exception exception) {
             System.out.println("Error in mainMenu method " + exception);
         }
     }
 
-    public static void checkMenu(String name){
-        if(raffelTickets.containsValue(name)){
-            if
+    public static void checkMenu(String name) {
+        //make bulletproof
+        int ticketNumber = 0;
+
+        if (raffelTickets.containsValue(name)) {
+            for (Entry<Integer, String> entry : raffelTickets.entrySet()) {
+                if (entry.getValue() == name) {
+                    ticketNumber = entry.getKey();
+                }
+            }
+            BigInteger userRaffelTicket = new BigInteger(String.valueOf(ticketNumber));
+            if (userRaffelTicket.isProbablePrime(1)) {
+                System.out.println("You won the raffle");
+                System.out.println("Yay");
+            } else {
+                System.out.println("Bad luck, you lost");
+                System.out.println("Try again?");
+            }
+        } else {
+            System.out.println("You need to buy a ticket to check one");
         }
     }
 
-    public static void buyMenu(String name){
+    public static void buyMenu(String name) {
+        Random random = new Random();
+        System.out.println("How many tickets would you like to buy?");
+        try {
+            Integer amountOfTickets = Integer.valueOf(getInput());
+            for (int i = 0; i <=amountOfTickets; i++) {
+                int randomTickets = random.nextInt(raffelTickets.size());
 
+            }
+        }catch(Exception e){
+            System.out.println("There was an error" + e);
+        }
     }
 
     private static HashMap<Integer, String> raffelTickets = tickets(500);
 
-    public static HashMap<Integer, String> tickets(int range){
+    public static HashMap<Integer, String> tickets(int range) {
         HashMap<Integer, String> tickets = new HashMap<>();
-        for (int i = 1; i <=range; i++) {
+        for (int i = 1; i <= range; i++) {
             tickets.put(i, "");
         }
         return tickets;
     }
 
-    public static String getInput(){
+    public static String getInput() {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        try{
+        try {
             return bufferedReader.readLine();
-        }catch (Exception exception){
+        } catch (Exception exception) {
             System.out.println("BufferedReader had an error" + exception);
             return "Cringe";
         }
